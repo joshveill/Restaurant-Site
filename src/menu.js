8b635b1e8ad350e819197151ff0f2576d06ec477
Joshua Veilleux <joshuaveilleux1@gmail.com>
2022-10-16 17:59:40 -0400

    menu ready to be filled, card effects and logic finished

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,62 @@
+function createMenu() {
+  
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menu-container");
+
+  menuContainer.appendChild(
+    makeMenuItem(
+      "Salmon Nigiri",
+      "Hand rolled salmon and fish roe sushi served with soy sauce"
+    )
+  );
+
+  menuContainer.appendChild(
+    makeMenuItem(
+      "Salmon Nigiri",
+      "Hand rolled salmon and fish roe sushi served with soy sauce"
+    )
+  );
+
+  menuContainer.appendChild(
+    makeMenuItem(
+      "Salmon Nigiri",
+      "Hand rolled salmon and fish roe sushi served with soy sauce"
+    )
+  );
+
+  return menuContainer;
+}
+
+function makeMenuItem(foodItem, description) {
+
+  const menuItem = document.createElement("div");
+  menuItem.classList.add("menu-item");
+
+  const foodName = document.createElement("h3");
+  foodName.classList.add("food-name");
+  foodName.textContent = foodItem;
+
+  const foodImage = document.createElement("img");
+  foodImage.classList.add("food-image");
+  foodImage.src = `images/${foodItem}.png`;
+
+  const foodDescription = document.createElement("p");
+  foodDescription.classList.add("food-description");
+  foodDescription.textContent = description;
+
+  menuItem.appendChild(foodName);
+  menuItem.appendChild(foodImage);
+  menuItem.appendChild(foodDescription);
+
+  return menuItem;
+}
+
 function loadMenu() {
+  
   const main = document.getElementById("main");
   main.textContent = "";
-  console.log("menu");
+
+  main.appendChild(createMenu());
 }
 
 export default loadMenu;
